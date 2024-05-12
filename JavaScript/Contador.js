166ea030d806f0b34bc78e7a954fc127e6aea336
CrisExe <cris.beforus@hotmail.com>
2024-05-12 18:07:33 -0400

    pequeños arreglos

--- JavaScript/Contador.js
+++ JavaScript/Contador.js
@@ -38,4 +38,4 @@ const countdown = (deadline) => {//Función que recibe la fecha límite, el elem
     timerUpdate = setInterval(updateCountdown, 1000);
 }
 
-countdown('May 12 2024 00:00:00 GMT-0400');//Se llama a la función countdown con la fecha límite, el elemento donde se mostrará el contador y el mensaje final
+countdown('May 17 2024 00:00:00 GMT-0400');//Se llama a la función countdown con la fecha límite, el elemento donde se mostrará el contador y el mensaje final
